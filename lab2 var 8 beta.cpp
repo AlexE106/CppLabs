a9dc31a83442cce5dfc8f3e22b55a443ae0fb692
Unknown <tkfycrbq@yandex.ru>
2017-09-20 20:41:10 +0500

    задание №2 beta
    задание №2 вариант 8

---
+++ lab2 var 8 beta.cpp
@@ -0,0 +1,38 @@
+
+//2. 8вариант №2	Дано действительное число а.Для функции f(x), график которой представлен на рисунке, вычислить f(a).
+
+#include "stdafx.h"
+
+#include <iostream>
+
+#include <cmath>
+
+
+
+using namespace std;
+
+int main()
+
+{
+
+	float x, y, a;
+
+	cout << "enter the a" << endl;
+
+	cin >> a;
+
+	if (a < 0) {  
+		y = -a;
+	}
+	else {  
+		y = -(pow(a, 2));
+	}
+
+
+	cout << y;
+
+	system("pause");
+
+	return 0;
+
+}
